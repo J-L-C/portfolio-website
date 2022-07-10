f5ace022c60caa9f47210e962c709d305f755673
Jesse Cooper <CLJcareer@outlook.com>
2022-07-10 18:35:35 -0500

    Update all info and yaml files with my information

---
+++ my-site/gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Jesse Cooper",
+    locale: "en",
+    title: "Jesse Cooper",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "dark-green",
+      },
+    },
+  ],
+}
